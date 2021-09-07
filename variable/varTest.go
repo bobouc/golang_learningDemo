9cc0bb208ebeeda8a85752332dc0b0ecea3d33eb
bobouc <liylipsky@gmail.com>
2021-09-07 22:30:16 +0800

    variable demo

---
+++ variable/varTest.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("variable test")
+
+	var numA int
+	numA = 10
+	fmt.Println(numA)
+
+	var numB int = 20
+	fmt.Println(numB)
+
+	var name = "cyber"
+	fmt.Println(name)
+
+	cyber := "good boy"
+	fmt.Println(cyber)
+
+}
