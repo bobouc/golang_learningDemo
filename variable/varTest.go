e9d1dfa39dee200a7c0eb8b28c0b6fd930c2d3cc
bobouc <liylipsky@gmail.com>
2021-09-08 00:00:49 +0800

    variable, const, enumeration

--- variable/varTest.go
+++ variable/varTest.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("variable test")
 
 	var numA int
@@ -17,5 +17,45 @@ func main(){
 
 	cyber := "good boy"
 	fmt.Println(cyber)
+
+	var m float64 //m: 0.0
+	fmt.Println(m)
+	var s string //""
+	fmt.Println(s)
+	var s1 []int
+	fmt.Println(s1)
+	fmt.Println(s1 == nil)
+
+	//constant variable
+	const PATH string = "http://www.google.com"
+	const PI = 3.1415925535
+	fmt.Println(PATH)
+	fmt.Println(PI)
+	// define a group of constant variable
+	const c1, c2, c3 = 100, 300, "test"
+	const (
+		MALE   = 0
+		FEMALE = 1
+		UNKNOW = 3
+	)
+	const (
+		a int = 100
+		b
+		c string = "const string"
+		d
+	)
+	fmt.Printf("%T, %d\n", a, a)
+	fmt.Printf("%T, %d\n", b, b)
+	fmt.Printf("%T, %s\n", c, c)
+	fmt.Printf("%T, %s\n", d, d)
+
+	//enumeration 
+	const(
+		SPRING = 0
+		SUMMER = 1
+		AUTUMN = 2
+		WINTER = 3
+	)
+	
 
 }
